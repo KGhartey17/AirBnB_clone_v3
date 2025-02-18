33cd457767ae8fa3ea67300625078b992fe56f14
KGhartey17 <kaphaelghartey@gmail.com>
2025-02-18 02:06:45 +0000

    base

--- models/base_model.py
+++ models/base_model.py
@@ -49,8 +49,8 @@ class BaseModel:
 
     def __str__(self):
         """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+        cls_name = self.__class__.__name__
+        return f"[{cls_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
