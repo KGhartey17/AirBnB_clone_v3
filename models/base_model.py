aa712bcebc7b5d4f76afce6a78c5ecac21018853
KGhartey17 <kaphaelghartey@gmail.com>
2025-02-18 02:22:47 +0000

    base model.py

--- models/base_model.py
+++ models/base_model.py
@@ -49,8 +49,8 @@ class BaseModel:
 
     def __str__(self):
         """String representation of the BaseModel class"""
-        cls_name = self.__class__.__name__
-        return f"[{cls_name}] ({self.id}) {self.__dict__}"
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
@@ -62,8 +62,7 @@ class BaseModel:
         """Returns a dictionary containing all keys/values of the instance.
 
         Args:
-            include_password (bool): If True, includes the password in the
-                dictionary.
+            include_password (bool): If True, includes the password in the dictionary.
         """
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
